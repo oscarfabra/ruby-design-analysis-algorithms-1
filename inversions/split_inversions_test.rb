41518f9a2e35016e2bb3752e49d7dcd5ef191e26
Oscar Fabra <oscarfabra@gmail.com>
2014-10-28 11:30:48 -0500

    Defined test cases for merge_and_count_split_inversions method.

--- inversions/split_inversions_test.rb
+++ inversions/split_inversions_test.rb
@@ -11,10 +11,15 @@ require './split_inversions'
 #------------------------------------------------------------------------------
 class SplitInversionsTest < MiniTest::Unit::TestCase
 
-  # Performs initializations for the test.
+  # Performs initializations for the tests.
   def setup
-    # Setup values...
-    # @lines = read_lines("./data/")
+    # Reads lines from file and arranges them in an array of integers
+    lines = read_lines("./data/inversions_5_1.txt")
+    @a = []
+    lines.each do |line|
+      @a << line.to_i
+    end
+    puts "#{@a}"
   end
 
   # Reads lines of given file and arranges them in a list. Returns a list of
@@ -23,22 +28,47 @@ class SplitInversionsTest < MiniTest::Unit::TestCase
   # Params:
   # +filename+:: Path relative to the file with the input for the problem.
   def read_lines(filename)
-    # TODO: Read lines from stdin and arrange them in a list.
+    lines = []
+    open(filename) do |file|
+      while line = file.gets
+        lines << line
+      end
+      lines
+    end
+  rescue StandardError => ex
+    # Throws exception if file couldn't be read
+    raise ex.class, "File couldn't be read."
   end
 
   # Tests that merge_and_count_split_inversions(a) method work as expected.
   def test_merge_and_count_split_inversions
-    # TODO: Write test...
+    a = []
+    b = [1, 3, 5]
+    c = [2, 4, 6]
+    split_inversions = merge_and_count_split_inversions(a, b, c, 0, 2, 5)
+    
+    # split_inversions should be 3, a should be [1, 2, 3, 4, 5, 6]
+    assert_equal 3, split_inversions
+    assert_equal [1, 2, 3, 4, 5, 6], a
+
+    a = []
+    b = [3, 4, 5]
+    c = [1, 2]
+    split_inversions = merge_and_count_split_inversions(a, b, c, 0, 2, 4)
+
+    # split_inversions should be 6, a should be [1, 2, 3, 4, 5]
+    assert_equal 6, split_inversions
+    assert_equal [1, 2, 3, 4, 5], a
   end
 
   # Tests that merge_and_count_split_inversions(a,lb,ub) work as expected.
-  def test_sort_and_count_bounded
+  #def test_sort_and_count_bounded
     # TODO: Write test...
-  end
+  #end
 
   # Test that main solver method work as expected for any list of numbers.
-  def test_sort_and_count
+  #def test_sort_and_count
     # TODO: Write test...
-  end
+  #end
 
 end
