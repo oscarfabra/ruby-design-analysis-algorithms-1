afb8579eef0222d623b418099a4910f51a974f03
Oscar Fabra <oscarfabra@gmail.com>
2014-11-12 22:29:54 -0500

    Defined test methods for vertex and edge classes.

---
+++ mincut/edge_test.rb
@@ -0,0 +1,24 @@
+gem 'minitest'
+require 'minitest/autorun'
+require './edge'
+
+#------------------------------------------------------------------------------
+# :section: edge_test.rb
+# This class tests that Edge class works as expected. Edge represents an edge
+# of the Graph class.
+# *author*:: <a href="mailto:oscarfabra@gmail.com">Oscar Fabra</a>
+# *version*:: 1.0
+# *since*:: 12/11/2014
+#------------------------------------------------------------------------------
+class EdgeTest < MiniTest::Test
+
+  # Performs initializations for tests
+  def setup
+    # TODO: Initialize values.
+  end
+
+  # Tests that initialize method works as expected.
+  def test_initialize
+    # TODO: Write method.
+  end
+end
---
+++ mincut/vertex_test.rb
@@ -0,0 +1,39 @@
+gem 'minitest'
+require 'minitest/autorun'
+require './vertex'
+
+#------------------------------------------------------------------------------
+# :section: edge_test.rb
+# This class tests that Vertex class works as expected. Vertex represents a
+# vertex or node of the Graph class.
+# *author*:: <a href="mailto:oscarfabra@gmail.com">Oscar Fabra</a>
+# *version*:: 1.0
+# *since*:: 12/11/2014
+#------------------------------------------------------------------------------
+class VertexTest < MiniTest::Test
+
+  # Performs initializations for tests
+  def setup
+    # TODO: Initialize values.
+  end
+
+  # Tests that initialize method works as expected.
+  def test_initialize
+    # TODO: Write method.
+  end
+
+  # Tests that clone method works as expected.
+  def test_clone
+    # TODO: Write method.
+  end
+
+  # Tests that include? method works as expected.
+  def test_include?
+    # TODO: Write method.
+  end
+
+  # Tests that merge_vertices method works as expected.
+  def test_merge_vertices
+    # TODO: Write method.
+  end
+end
